5bfc1a10df36d35e0dacb2c2f0e17ecb085f7e7f
Skullcr1 <kornelismak@gmail.com>
2019-02-23 23:28:55 +0200

    improved readability, dability,got rid of needless code, everything seems to work, empty list creation missing (prob. it's necessary)

--- assigments/dequeMain.c
+++ assigments/dequeMain.c
@@ -1,16 +1,17 @@
 #include <stdio.h> 
 #include <stdlib.h> 
   
-// A linked list node 
+ 
 struct Node 
 { 
   int data; 
   struct Node *next;
   struct Node *previous; 
-}; 
+} *head; 
   
-/* inserts a new node on the front of the list. */
-void push(struct Node** head_ref, int new_data) 
+/* inserts a new node at the start */
+
+void addfront(struct Node** head_ref, int new_data) // adds a new node at the beggining 
 { 
     /* allocate node */
     struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
@@ -24,14 +25,14 @@ void push(struct Node** head_ref, int new_data)
   
     /* move the head to point to the new node */
     (*head_ref)    = new_node; 
-} 
+    } 
   
-/* adds a new node at the end  */
-void append(struct Node** head_ref, int new_data) 
+
+void addend(struct Node** head_ref, int new_data) //adds at end
 { 
     /* allocate node */
     struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
-    struct Node *tail = *head_ref;  /* used to get last element*/
+    struct Node *tail = *head_ref;  
   
     /* put in the data  */
     new_node->data  = new_data; 
@@ -40,10 +41,7 @@ void append(struct Node** head_ref, int new_data)
           it as NULL*/
     new_node->next = NULL; 
 
-    /* previous node points to nothing */
-    new_node->previous = *(head_ref);
-
-    /* if the list is empty, then make the new node as head */
+    /* if the deque is empty, then make the new node as head */
     if (*head_ref == NULL) 
     { 
        *head_ref = new_node; 
@@ -51,95 +49,91 @@ void append(struct Node** head_ref, int new_data)
     } 
 
     /* traverse till the last node */
-    while (tail->next != NULL)     
+    while (tail->next != NULL)
+    {     
         tail = tail->next; 
+    }
+
     /* change the next of last node */
     tail->next = new_node; 
     new_node->previous = tail;
     return; 
-} 
+    } 
 
-void DELETE_FRONT(struct Node **head_ref) //front
+void delfront(struct Node **head_ref) //deletes front node
 {
     struct Node *current = *head_ref;
 
-    if(current == NULL)
+    if(current == NULL) //checkig if there is no node
     {
         printf("list is already empty");
+        return;
     }
 
-    while (current != NULL) //is necessery
-        { 
-            *head_ref = current->next;    
-            free(current);             
-            return;
-        }   
-       
-    }
+    *head_ref = current->next;    //2nd node - head 
+    free(current);                //deleting 1st node
+    }   
 
-
-void DELETE_REAR(struct Node **head_ref) //works
+void delrear(struct Node **head_ref) //deletes last node
 {
     struct Node *current = *head_ref;
     struct Node *last = *head_ref;
     struct Node *check = *head_ref;
    
-    
-    if(*head_ref == NULL)
+    if(*head_ref == NULL)           //checking if there is no node
     {
         printf("already empty list");
         return;
     }
-    // printf("already empty list");
-        while(current ->next !=NULL)
-        {
-            last = current;
-            current = current ->next;
-            current ->previous = check;
-        }
-    
-        
-        if(current == *head_ref)
-        {
-            *head_ref = NULL;
-            free(current);
-            return;
-        }
-        last ->next = NULL;
+
+    while(current ->next !=NULL)    //traversing to the end of deque
+    {
+        last = current;
+        current = current ->next;
+        current ->previous = check;
+    }
+      
+    if(current == *head_ref)        //if current is last one we make head NULL and delete node
+    {
+        *head_ref = NULL;
         free(current);
-        
+        return;
+    }
+
+    last ->next = NULL;             //last node loses connection with previous
+    free(current);
         }
 
 
 
     
 
-void DELETE_ALL(struct Node **head_ref)
+void delall(struct Node **head_ref) //deletes everything (starting at beggining)
 {
      struct Node *current = *head_ref;
      struct Node *next; 
 
-    if(current == NULL)
+    if(current == NULL)             //checking if there is no node
     {
         printf("already empty list");
     }
 
 
-     while (current != NULL)  
+     while (current != NULL)        //traversing and deleting nodes
         { 
-            next = current->next; 
+            next = current->next;   //
             free(current); 
             current->previous = NULL;
             current = next; 
         } 
     
    
-    *head_ref = NULL; 
+    *head_ref = NULL;               //if there's everything already deleted we point head to NULL
         } 
         
 
 
-void printList(struct Node *node) 
+void printdeque(struct Node *node) //function to print deque elements (to check correctness)
 { 
   while (node != NULL) 
   { 
@@ -151,26 +145,14 @@ void printList(struct Node *node)
   
 int main() 
 { 
-  struct Node* tail = NULL;
-  struct Node* head = NULL; 
-  
-  push(&head, 7); 
-  //push(&head, 1); 
-//   append(&head, 4); 
-  append(&head, 5);
-//   DELETE_FRONT(&head);
-  DELETE_REAR(&head);
-  DELETE_REAR(&head);
-
-  push(&head, 1);
-//   DELETE_ALL(&head);
-//   DELETE_ALL(&head);
-//   push(&head, 1);
-
-  
-  
+      
+  addfront(&head, 1); 
+  addend(&head, 4); 
+  addend(&head, 5);
+//   delfront(&head);
+  delall(&head);
   printf("\n list is: "); 
-  printList(head); 
+  printdeque(head); 
   
   return 0; 
 } 
